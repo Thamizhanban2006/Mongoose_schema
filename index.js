556734561d8cc340a9dac2060e6bbf33a2f554d5
ThamizhAnban <thamizhanban2006@gmail.com>
2025-02-11 11:37:34 +0530

    schema created

--- index.js
+++ index.js
@@ -13,3 +13,37 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+const mongoose = require("mongoose");
+const User = require("./schema.js"); 
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/mydatabase", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.error("Connection error", err));
+
+
+const testUserSchema = async () => {
+  try {
+    const newUser = new User({
+      username: "Thamizh_anban",
+      email: "Thamizhanban@gmail.com",
+      password: "thamizh@123456789",
+      profile: {
+        firstName: "Thamizh",
+        lastName: "anban",
+        age: 25
+      }
+    });
+
+    await newUser.save();
+    console.log("User saved successfully:", newUser);
+  } catch (error) {
+    console.error("Error saving user:", error);
+  }
+};
+
+
+testUserSchema();
